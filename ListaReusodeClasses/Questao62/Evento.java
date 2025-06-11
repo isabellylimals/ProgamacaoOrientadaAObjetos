9d6d6f1ef76c7a02ce98f7e59439c4101d5a009b
Isabelly Lima <mslima5643@gmail.com>
2025-06-11 16:48:54 -0300

    up

--- ListaReusodeClasses/Questao62/Evento.java
+++ ListaReusodeClasses/Questao62/Evento.java
@@ -2,7 +2,7 @@ package ListaReusodeClasses.Questao62;
 
 public class Evento {
     public static void main(String[] args) {
-         EventoDelegacao evento= new EventoDelegacao("Festa", "01/01/2023", 20);
+         EventoDelegacao evento= new EventoDelegacao("Festa", "01/01/2023", 20);//lembrar de trocar hora para string
         EventoHeranca eventoHeranca = new EventoHeranca("Festa", "01/01/2023", 20);
         System.out.println(evento);
         System.out.println(eventoHeranca);         
